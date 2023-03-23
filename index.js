c9b701b99c4c283b11a6c123d845a58c18a15c61
Dhanush <99753690+Dbdhanush@users.noreply.github.com>
2023-03-23 15:31:04 +0530

    darkmood succussfully added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const toggler =document.getElementById('darkmood');
 const body =document.querySelector('body');
 const content =document.querySelector('h5');
-const a =document.querySelector('h6');
+const h6 =document.querySelector('h6');
+const a =document.querySelector('a');
 toggler.addEventListener('click', function(){
     toggler.classList.toggle('fa-moon');
     if(toggler.classList.toggle('fa-sun'))
@@ -10,7 +11,8 @@ toggler.addEventListener('click', function(){
         body.style.color='black';
         body.style.transition='1s';
         content.style.color='black';
-        a.style.color='black';
+        a.style.color='blue';
+        h6.style.color='black';
     }
     else{
         {
@@ -19,7 +21,7 @@ toggler.addEventListener('click', function(){
             body.style.transition='1s';
             content.style.color='white';
             a.style.color='white';
-       
+            h6.style.color='white';
         }
     }
 })
